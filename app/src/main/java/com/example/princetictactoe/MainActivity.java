78f11b19e335aab2b4709745ede00afa9ae30df0
PRINCEKUMAR2025 <princeiitbombay@gmail.com>
2022-12-17 00:20:16 +0530

    Tic Tac Toe Colour2.0

--- app/src/main/java/com/example/princetictactoe/MainActivity.java
+++ app/src/main/java/com/example/princetictactoe/MainActivity.java
@@ -35,12 +35,12 @@ public class MainActivity extends AppCompatActivity {
                 img.setImageResource(R.drawable.x);
                 activePlayer = 1;
                 TextView status = findViewById(R.id.status);
-                status.setText("Prince says O`s Turn-Tap");
+                status.setText("Hey! its O`s Turn-Tap");
             } else {
                 img.setImageResource(R.drawable.o);
                 activePlayer = 0;
                 TextView status = findViewById(R.id.status);
-                status.setText("Prince says X`s Turn-Tap");
+                status.setText("Hey! its X`s Turn-Tap");
             }
 
             img.animate().translationYBy(1000f).setDuration(300);
